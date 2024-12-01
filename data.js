0c8e3191a184c0c5bf0603bc9a64f927caee873e
dev-nathaniel <119062406+dev-nathaniel@users.noreply.github.com>
2024-12-01 22:33:14 +0000

    Add files via upload

---
+++ data.js
@@ -0,0 +1,162 @@
+export const workSlideShow = [
+    // {
+    //     id: 1,
+    //     cover: 'abstract1.webp',
+    //     title: 'TRINAC',
+    //     link: ''
+    // },
+    // {
+    //     id: 2,
+    //     cover: 'abstract2.jpg',
+    //     title: 'WOMAN',
+    //     link: ''
+    // },
+    // {
+    //     id: 3,
+    //     cover: 'abstract3.jpg',
+    //     title: 'PORTFOLIO',
+    //     link: ''
+    // },
+    // {
+    //     id: 4,
+    //     cover: 'abstract4.jpg',
+    //     title: 'MUSHROOM',
+    //     link: ''
+    // },
+    {
+        id: 5,
+        cover: 'kyra.png',
+        title: 'KYRA',
+        link: 'https://kyra.netlify.app'
+    },
+    {
+        id: 6,
+        cover: 'music.png',
+        title: 'MUSIC PLAYER',
+        link: 'https://music-olowo.netlify.app'
+    },
+    {
+        id: 7,
+        cover: 'shuttl.png',
+        title: 'SHUTTL',
+        link: 'https://shuttl.app'
+    },
+    {
+        id: 8,
+        cover: 'orpheus.png',
+        title: 'ORPHEUS',
+        link: 'https://orpheus-nft-site.netlify.app'
+    },
+]
+
+export const workGallery = [
+    // {
+    //     id: 1,
+    //     cover: 'abstract11.webp',
+    //     title: 'TRINAC',
+    //     link: ''
+    // },
+    // {
+    //     id: 2,
+    //     cover: 'abstract21.jpg',
+    //     title: 'WOMAN',
+    //     link: ''
+    // },
+    // {
+    //     id: 3,
+    //     cover: 'abstract31.jpg',
+    //     title: 'PORTFOLIO',
+    //     link: ''
+    // },
+    // {
+    //     id: 4,
+    //     cover: 'abstract41.jpg',
+    //     title: 'MUSHROOM',
+    //     link: ''
+    // },
+    {
+        id: 5,
+        cover: 'abstract1.webp',
+        title: 'ORPHEUS',
+        link: 'https://orpheus-nft-site.netlify.app'
+    },
+    {
+        id: 6,
+        cover: 'abstract2.jpg',
+        title: 'MUSIC PLAYER',
+        link: 'https://music-olowo.netlify.app'
+    },
+    {
+        id: 7,
+        cover: 'abstract3.jpg',
+        title: 'GIS 3D APP',
+        link: 'https://gis-vanilla.netlify.app'
+    },
+    {
+        id: 8,
+        cover: 'abstract4.jpg',
+        title: 'DECAL',
+        link: 'https://thedecalmasters.com'
+    },
+]
+
+
+export const services = [
+    {
+        id: 1,
+        title: 'Website',
+    },
+    {
+        id: 1,
+        title: 'Front-end Development',
+    },
+    {
+        id: 1,
+        title: 'Back-end Development',
+    },
+    {
+        id: 1,
+        title: 'App Development',
+    },
+]
+
+export const budgetRanges = [
+    {
+        id: 1,
+        title: 'Less than $500'
+    },
+    {
+        id: 2,
+        title: '$500 - $2K'
+    },
+    {
+        id: 3,
+        title: '$2K - $5K'
+    },
+    {
+        id: 4,
+        title: 'More than $5K'
+    },
+]
+
+export const deliveryPeriod = [
+    {
+        id: 1,
+        title: 'Urgent'
+    },
+    {
+        id: 2,
+        title: '3 months'
+    },
+    {
+        id: 3,
+        title: '6 months'
+    },{
+        id: 4,
+        title: '9 months'
+    },
+    {
+        id: 5,
+        title: 'To be discussed'
+    },
+]
